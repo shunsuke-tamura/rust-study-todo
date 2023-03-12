5281dfd888132fe39092292ca15d0fe7e3ad2346
shunsuke-tamura <wwwxx4869@gmail.com>
2023-03-13 04:06:10 +0900

    update: ocrできた

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    println!("hello!");
-    for n in 1..11 {
-        println!("{n}Hello, world!");
-    }
+    let mut lt = leptess::LepTess::new(None, "eng").unwrap();
+    lt.set_image("test.png").unwrap();
+    println!("{}", lt.get_utf8_text().unwrap());
+    // println!("hello!");
+    // for n in 1..11 {
+    //     println!("{n}Hello, world!");
+    // }
 }
